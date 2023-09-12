65fa9f0a14da9351ec2931cdb4db4438e0389d03
leticia <123770407+lets02@users.noreply.github.com>
2023-09-12 14:44:59 -0300

    Add files via upload

--- CalculadoraDioguetes/src/App.java
+++ CalculadoraDioguetes/src/App.java
@@ -1,11 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        //new CalculadoraFlowLayout().setVisible(true); 
-        // Calc calc = new Calc();
-        // calc.setVisible( true ); 
-         //Calculadora3 calculadora = new Calculadora3();
-         //calculadora.setVisible( true ); 
-         Calculadora4 exemplo = new Calculadora4();
-         exemplo.setVisible(true);
+         ///Calc calc = new Calc();
+         //calc.setVisible( true ); 
+         //Calculadora4 exemplo = new Calculadora4();
+         //exemplo.setVisible(true);
+         new CalculadoraConversor();
     }
 }
